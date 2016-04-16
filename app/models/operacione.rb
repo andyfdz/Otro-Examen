0833f3b6f840c23ae541388b57f576990bcf8b46
andyfdz <andrea.fdez.rojas@gmail.com>
2016-04-15 21:48:34 -0500

    alterar modelo operacione agregando item

--- app/models/operacione.rb
+++ app/models/operacione.rb
@@ -1,6 +1,6 @@
 class Operacione < ActiveRecord::Base
 	belongs_to :trabajadore
 	has_many :inventario, dependent: :destroy
-	scope :published_recent, ->{order("Operacione.updated_at Desc", >5)}
+	scope :published_recent, ->{order("Operacione.updated_at Desc")}
 	scope :published_last_day, ->{where("published_at >= ?", 1.day.ago) }
 end
--- db/migrate/20160416012759_create_operaciones.rb
+++ db/migrate/20160416012759_create_operaciones.rb
@@ -2,6 +2,7 @@ class CreateOperaciones < ActiveRecord::Migration
   def change
     create_table :operaciones do |t|
       t.string :realizado_por
+      t.string :item
 
       t.timestamps null: false
     end
