8425ce43dee6df60127dc0e2f1fa2e84c49dd12a
andyfdz <andrea.fdez.rojas@gmail.com>
2016-04-15 21:27:42 -0500

    scopes

--- app/models/inventario.rb
+++ app/models/inventario.rb
@@ -1,4 +1,6 @@
 class Inventario < ActiveRecord::Base
-	belongs_to :operacione
+	belongs_to :operacione dependent: :destroy
 	validates :serie_id presence: true, uniqueness: true, not null
+	scope :published, ->{where(completed: first_user) }
+	scope :incompleted, ->{where(completed: false) if user_do_not_use }
 end
