84ae066d2055c37f04dcfb77e4ff55652553f6d0
andyfdz <andrea.fdez.rojas@gmail.com>
2016-04-15 22:24:02 -0500

    ultimo

--- app/models/inventario.rb
+++ app/models/inventario.rb
@@ -1,6 +1,11 @@
 class Inventario < ActiveRecord::Base
-	belongs_to :operacione dependent: :destroy
-	validates :serie_id presence: true, uniqueness: true, not null
+	belongs_to :operacione 
+	dependent: :destroy
+
+	validates :serie_id 
+	presence: true, 
+	uniqueness: true
+	
 	scope :published, ->{where(completed: first_user) }
 	scope :incompleted, ->{where(completed: false) if user_do_not_use }
 end
